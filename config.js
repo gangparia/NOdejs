c4fc7d456092f2e2a12274ae9681da0b33746893
Pankaj Kumar <pankaj8261@gmail.com>
2017-08-18 01:20:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
-config.userName = 'pankaj@sharepoint.com';
-config.pwd = '123topcoder54iMFCJsvAwHHWpUhAob6hunF';
+config.userName = 'pankaj@*************.com';
+config.pwd = '123*********************';
 config.accountQuery = 'SELECT Id, Name,(SELECT id ,CreatedDate , Name FROM Attachments) FROM Account';
 config.folderDir = '/';
 module.exports = config;

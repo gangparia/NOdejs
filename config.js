ec4e87f9b5c70d9e6e3ac7535969817d3f54b717
Pankaj Kumar <pankaj8261@gmail.com>
2017-08-20 16:30:10 +0530

    contact query added

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ var config = {};
 config.userName = 'pankaj@*************.com';
 config.pwd = '123*********************';
 config.accountQuery = 'SELECT Id, Name,(SELECT id ,CreatedDate , Name FROM Attachments) FROM Account';
+config.contactQuery = 'SELECT Id, Name, AccountId, Account.Name,(SELECT id ,CreatedDate , Name FROM Attachments) FROM Contact where AccountId !=null';
 config.folderDir = '/';
 module.exports = config;

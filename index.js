9906e431f8384ea96d22019fc97eae5c91caf93b
Pankaj Kumar <pankaj8261@gmail.com>
2017-08-20 16:29:28 +0530

    queryMore issue resolved

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ var jsforce = require('jsforce');
 var config = require('./config');
 var log4js = require('log4js');
 
+//global variables
+var accountRecordCount = 0;
+var contactRecordCount = 0;
+var numberOfAttachmentCount = 0;
+var numberOfFolderCreated = 0;
+var isDownloadFiles = true;
+var conn = new jsforce.Connection();
+
 // timestamp for unique fileName
 var timestamp = new Date().getTime().toString();
 var logFilePath = config.folderDir + 'site_' + timestamp + '.log';
@@ -25,41 +33,64 @@ log4js.configure({
 });
 var logger = log4js.getLogger();
 
-
 // connection to salesforce
-console.log(config.userName);
+logger.info(config.userName);
 console.log(config.pwd);
-var conn = new jsforce.Connection();
+
 conn.login(config.userName, config.pwd, function(err, res) {
     if (err) {
         return console.error(err);
     }
-    console.log(config.accountQuery);
-    conn.query(config.accountQuery, function(err, res) {
+    downloadAccountFiles();
+});
+
+//quey all acccount records 
+function downloadAccountFiles() {
+    queryMoreFunction(config.accountQuery, 'Account');
+}
+
+// query all contact records
+function downloadContactFiles() {
+    queryMoreFunction(config.contactQuery, 'Contact');
+}
+
+//query method to fetch records from objects
+function queryMoreFunction(originalQuery, objectName) {
+    logger.info('originalQuery--> ' + originalQuery);
+    logger.info('objectName--> ' + objectName);
+    var records = [];
+    var handleResult = function(err, res) {
         if (err) {
-            return console.error(err);
-        } else {
-            records.push.apply(records, res.records);
-            if (res.done) {
+            logger.warn(err);
+            callback(err);
+        }
+        records.push.apply(records, res.records);
+        if (res.done) {
+            if (objectName == 'Account') {
+                console.log('Account callback called');
                 callbackAccount(null, {
                     result: res,
                     records: records
                 });
-            } else {
-                query = conn.queryMore(res.nextRecordsUrl, handleResult);
+            } else if (objectName == 'Contact') {
+                console.log('Contact callback called');
+                callbackContact(null, {
+                    result: res,
+                    records: records
+                });
             }
+        } else {
+            query = conn.queryMore(res.nextRecordsUrl, handleResult);
         }
-    });
-});
+    };
+    var query = conn.query(originalQuery, handleResult);
+    var callback = function(err, result) {
+        if (err) {
+            throw err;
+        }
+    };
+}
 
-
-var records = [];
-var dirIndexMap = new Map();
-var accountRecordCount = 0;
-var contactRecordCount = 0;
-var numberOfAttachmentCount = 0;
-var numberOfFolderCreated = 0;
-var isDownloadFiles = true;
 
 //fetch all account attachments
 var callbackAccount = function(err, result) {
@@ -72,28 +103,61 @@ var callbackAccount = function(err, result) {
     for (var i = res.records.length - 1; i >= 0; i--) {
         var AccountName = (res.records[i].Name).replace(',', '').replace('.', '_').replace(/ /g, '_');
         var dir = config.folderDir + AccountName;
-        dirIndexMap.set(res.records[i].Id, dir);
         if (res.records[i].Attachments != null) {
             accountRecordCount++;
             if (!fs.existsSync(dir)) {
-                console.log('creating directory' + i);
+                //console.log('creating directory' + i);
                 isDownloadFiles = false;
                 var index = i;
                 numberOfFolderCreated++;
                 fs.mkdir(dir, index, function(err, responseDir) {
                     if (err) {
-                        console.log('error in mkdir: ' + err);
+                        logger.warn('error in mkdir: ' + err);
                     }
                 });
-                logger.info('New Directory :' + dir);
+                logger.info('New Directory from account :' + dir);
             } else {
                 downloadFile(res, dir, i, 'Account Attachment :');
             }
         } //end if
     }
+    downloadContactFiles();
+}
 
-    downloadContactFiles(dirIndexMap);
+//fetch All contact Files
+var callbackContact = function(err, result) {
+    if (err) {
+        logger.warn('error in contact callback ' + err);
+        throw err;
+    }
+    var res = result;
+    logger.info('Number of Contacts: ' + res.records.length);
+    var contactRecordCount = 0;
+    for (var i = res.records.length - 1; i >= 0; i--) {
+        if (res.records[i] && res.records[i].Attachments && res.records[i].AccountId) {
+            contactRecordCount++;
+            var AccountName = (res.records[i].Account.Name).replace(',', '').replace('.', '_').replace(/ /g, '_');
+            var dir = config.folderDir + AccountName;
+            if (!fs.existsSync(dir)) {
+                numberOfFolderCreated++;
+                var index = i;
+                fs.mkdir(dir, index, function(err, responseDir) {
+                    if (err) {
+                        logger.warn('error in mkdir: ' + err);
+                    }
+                });
+                logger.info('New Directory from contact :' + dir);
+            } else if (isDownloadFiles) {
+                downloadFile(res, dir, i, 'Contact Attachment :');
+            }
+        } //end if
+    }
 
+    logger.info('Number of account record having attachments : ' + accountRecordCount);
+    logger.info('Number of contact record having attachments : ' + contactRecordCount);
+    logger.info('Number of folder created : ' + numberOfFolderCreated);
+    logger.info('Number of  attachments downloaded : ' + numberOfAttachmentCount);
+    console.log('process finished');
 }
 
 // download file either from Account attachment or contact attachment
@@ -113,67 +177,4 @@ function downloadFile(res, dir, i, msg) {
             }
         } //for
     }
-}
-
-// fetch all contact attachment 
-function downloadContactFiles(dirIndexMap) {
-    var accountIds = '(';
-    for (var key of dirIndexMap.keys()) {
-        accountIds += "'" + key + "',";
-    }
-    accountIds = accountIds.substring(0, accountIds.length - 1) + ')';
-    //console.log(accountIds);
-    conn.query('SELECT Id, Name, AccountId, Account.Name,(SELECT id ,CreatedDate , Name FROM Attachments) FROM Contact where accountId IN ' + accountIds, function(err, res) {
-        if (err) {
-            logger.warn('error in contact attachment query :  ' + err);
-            return console.error(err);
-        } else {
-            records.push.apply(records, res.records);
-            if (res.done) {
-                callbackContact(null, {
-                    result: res,
-                    records: records,
-                });
-
-            } else {
-                query = conn.queryMore(res.nextRecordsUrl, handleResult);
-            }
-        }
-    });
-}
-
-var callbackContact = function(err, result) {
-    if (err) {
-        logger.warn('error in contact callback ' + err);
-        throw err;
-    }
-    var res = result;
-    logger.info('Number of Contacts: ' + res.records.length);
-    var contactRecordCount = 0;
-    for (var i = res.records.length - 1; i >= 0; i--) {
-        if (res.records[i] && res.records[i].Attachments && res.records[i].AccountId) {
-            contactRecordCount++;
-            var AccountName = (res.records[i].Account.Name).replace(',', '').replace('.', '_').replace(/ /g, '_');
-            var dir = config.folderDir + AccountName;
-            dirIndexMap.set(res.records[i].Id, dir);
-            if (!fs.existsSync(dir)) {
-                numberOfFolderCreated++;
-                var index = i;
-                fs.mkdir(dir, index, function(err, responseDir) {
-                    if (err) {
-                        logger.warn('error in mkdir: ' + err);
-                    }
-                });
-                logger.info('New Directory :' + dir);
-            } else if (isDownloadFiles) {
-                downloadFile(res, dir, i, 'Contact Attachment :');
-            }
-        } //end if
-    }
-
-    logger.info('Number of account record having attachments : ' + accountRecordCount);
-    logger.info('Number of contact record having attachments : ' + contactRecordCount);
-    logger.info('Number of folder created : ' + numberOfFolderCreated);
-    logger.info('Number of  attachments downloaded : ' + numberOfAttachmentCount);
-    console.log('process finished');
 }
